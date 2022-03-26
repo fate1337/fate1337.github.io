9a74d7ce7a77f94e5ab100f7ab490b558c66567a
Денис <93658572+fate1337@users.noreply.github.com>
2022-03-26 20:35:08 +0300

    Add files via upload

--- script/menuActive.js
+++ script/menuActive.js
@@ -1,7 +1,7 @@
-const list= document.querySelectorAll('.list');
-const scr = document.querySelectorAll('.scr');
+const list = document.querySelectorAll('.list');
+const scr = document.querySelectorAll('.screen');
+const textUp = document.querySelector('.screen__up')
 let link = 0;
-console.log(list);
 function activeLink(){
     list.forEach((item) =>
     item.classList.remove('active'));
@@ -11,11 +11,13 @@ function activeLink(){
     item.classList.remove('activeOps'));
     if (link == 0){
         scr[0].classList.add('activeOps');
-        console.log(scr[0]);
+        textUp.classList.add('activeTitle');
     }else if (link == 1){
         scr[1].classList.add('activeOps');
+        textUp.classList.remove('activeTitle');
     }else if (link == 2){
         scr[2].classList.add('activeOps');
+        textUp.classList.remove('activeTitle');
     }
 }
 
