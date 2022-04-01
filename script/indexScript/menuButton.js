198fdeeb4808eb65689c0afb1c2173918723f02c
Денис <93658572+fate1337@users.noreply.github.com>
2022-04-01 21:01:48 +0300

    perfecting directory

---
+++ script/indexScript/menuButton.js
@@ -0,0 +1,21 @@
+const buttonForHero = document.querySelectorAll('.screen__link');
+
+let but;
+function forHero(){
+    list.forEach((item) =>
+    item.classList.remove('navigation__point_active'));
+    scr.forEach((item) =>
+    item.classList.remove('screen__body_active'));
+    if(buttonForHero[0] == but){
+    list[1].classList.add('navigation__point_active');
+    scr[1].classList.add('screen__body_active');
+    } else if(buttonForHero[1] == but){
+        list[2].classList.add('navigation__point_active');
+        scr[2].classList.add('screen__body_active'); 
+    }
+    textUp.classList.remove('screen__up_active');
+}
+
+buttonForHero.forEach((item) => item.addEventListener('click', function(){but = item}));
+
+buttonForHero.forEach((item) => item.addEventListener('click', forHero));
