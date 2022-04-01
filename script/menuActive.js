dcfc764e98769b45c83e2223743418fda685b3d4
Денис <93658572+fate1337@users.noreply.github.com>
2022-04-01 20:46:25 +0300

    perfecting texture

--- script/menuActive.js
+++ script/menuActive.js
@@ -1,23 +1,23 @@
-const list = document.querySelectorAll('.list');
-const scr = document.querySelectorAll('.screen');
+const list = document.querySelectorAll('.navigation__point');
+const scr = document.querySelectorAll('.screen__body');
 const textUp = document.querySelector('.screen__up');
 let link = 0;
 function activeLink(){
     list.forEach((item) =>
-    item.classList.remove('active'));
-    this.classList.add('active');
+    item.classList.remove('navigation__point_active'));
+    this.classList.add('navigation__point_active');
 
     scr.forEach((item) =>
-    item.classList.remove('activeOps'));
+    item.classList.remove('screen__body_active'));
     if (link == 0){
-        scr[0].classList.add('activeOps');
-        textUp.classList.add('activeTitle');
+        scr[0].classList.add('screen__body_active');
+        textUp.classList.add('screen__up_active');
     }else if (link == 1){
-        scr[1].classList.add('activeOps');
-        textUp.classList.remove('activeTitle');
+        scr[1].classList.add('screen__body_active');
+        textUp.classList.remove('screen__up_active');
     }else if (link == 2){
-        scr[2].classList.add('activeOps');
-        textUp.classList.remove('activeTitle');
+        scr[2].classList.add('screen__body_active');
+        textUp.classList.remove('screen__up_active');
     }
 }
 
--- script/menuButton.js
+++ script/menuButton.js
@@ -3,21 +3,19 @@ const buttonForHero = document.querySelectorAll('.screen__link');
 let but;
 function forHero(){
     list.forEach((item) =>
-    item.classList.remove('active'));
+    item.classList.remove('navigation__point_active'));
     scr.forEach((item) =>
-    item.classList.remove('activeOps'));
+    item.classList.remove('screen__body_active'));
     if(buttonForHero[0] == but){
-    list[1].classList.add('active');
-    scr[1].classList.add('activeOps');
+    list[1].classList.add('navigation__point_active');
+    scr[1].classList.add('screen__body_active');
     } else if(buttonForHero[1] == but){
-        list[2].classList.add('active');
-        scr[2].classList.add('activeOps'); 
+        list[2].classList.add('navigation__point_active');
+        scr[2].classList.add('screen__body_active'); 
     }
-    textUp.classList.remove('activeTitle');
+    textUp.classList.remove('screen__up_active');
 }
 
-console.log(buttonForHero[0]);
-console.log(buttonForHero[1]);
 buttonForHero.forEach((item) => item.addEventListener('click', function(){but = item}));
 
 buttonForHero.forEach((item) => item.addEventListener('click', forHero));
